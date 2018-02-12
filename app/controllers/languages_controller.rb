4e731619c2675e7486accd0dd92c77ea176d30fd
PMMWebDevelopment <pmmeddings@gmail.com>
2018-02-12 18:49:32 +0000

    Added Devise for User authentication

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,19 +1,25 @@
 class LanguagesController < ApplicationController
-  before_action :set_language, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_language, only: [:show, :edit, :update, :destroy, :feeds]
+  
   # GET /languages
   # GET /languages.json
   def index
     @languages = Language.all
   end
-
+  
   # GET /languages/1
   # GET /languages/1.json
   def show
     @allfeeds = Preloadsource.where(language: @language)
-    @selectedfeeds = @allfeeds.where(defaultbool: true)
-    @otherfeedsavailable = @allfeeds.where(defaultbool: false)
-  end
+    @submittagresults = params[:feed_ids]
+    if !@submittagresults
+      @selectedfeeds = @allfeeds.where(defaultbool: true)
+      @otherfeedsavailable = @allfeeds.where(defaultbool: false)
+    else
+      @selectedfeeds = @allfeeds.where(id: @submittagresults)
+      @otherfeedsavailable = @allfeeds.where.not(id: @submittagresults)
+    end
+  end 
 
   # GET /languages/new
   def new
@@ -27,24 +33,19 @@ class LanguagesController < ApplicationController
   # POST /languages
   # POST /languages.json
   def create
-    @language = Language.new(language_params)
+    # @language = Language.new(language_params)
 
-    respond_to do |format|
-      if @language.save
-        format.html { redirect_to @language, notice: 'Language was successfully created.' }
-        format.json { render :show, status: :created, location: @language }
-      else
-        format.html { render :new }
-        format.json { render json: @language.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @language.save
+    #     format.html { redirect_to @language, notice: 'Language was successfully created.' }
+    #     format.json { render :show, status: :created, location: @language }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @language.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
-
-  def feeds
-  #   respond_to do |format|
-  #   @selectedfeeds = 
-  end
-
+  
   # PATCH/PUT /languages/1
   # PATCH/PUT /languages/1.json
   def update
@@ -77,6 +78,7 @@ class LanguagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def language_params
-      params.require(:language).permit(:language)
+      params.permit(:language)
     end
-end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
-Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  resources :preloadsources
-  resources :sourcetypes
-  resources :languages do
-    member do
-      post 'feeds'
-    end
+  Rails.application.routes.draw do
+  devise_for :users
+    root :to => 'home#index'
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
+    resources :preloadsources
+    resources :sourcetypes
+    resources :languages
   end
-  root :to => 'home#index'
 
-
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
